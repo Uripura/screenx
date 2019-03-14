817da2354dbffc85392654b98576c4e74271092d
Uripura <uripura@uripura.de>
2019-03-14 15:58:15 +0000

    Add config to use a temp directory
    Also remove a old file

--- screenx.sh
+++ screenx.sh
@@ -18,32 +18,35 @@ sdir="/var/www/dir/to/img/folder"
 # Remote dir, the path for the browser
 rdir="/dir/to/img/folder/"
 
+# Temp Save Dir
+var="/var/tmp/"
+
 # Generate filename
 name="$(date +'%Y-%m-%d_%H-%M')_$((1000 + RANDOM % 9000)).png"
 
 # Region
 case "$1" in
 	1)
-	xfce4-screenshooter -r -s ~/$name
+	xfce4-screenshooter -r -s $var$name
 	;;
 
 # Active Window
 	2)
-	xfce4-screenshooter -w -s ~/$name
+	xfce4-screenshooter -w -s $var$name
 	;;
 
 # Entire Screen
 	3)
-	xfce4-screenshooter -f -s ~/$name
+	xfce4-screenshooter -f -s $var$name
 	;;
 
 # Default: only region
 	*)
-	xfce4-screenshooter -r -s ~/$name
+	xfce4-screenshooter -r -s $var$name
 esac
 
 lftp -c "open -u $user,$pass $rserver; put -O $sdir ~/$name"
 
-rm $name
+rm $var$name
 
 echo $server$rdir$name | xclip -selection c
